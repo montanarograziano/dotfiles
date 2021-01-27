b5ad8b7e62adb24849aafc1eabeb17492f4f595d
Graziano Montanaro <graziano.montanaro98@gmail.com>
2021-01-27 11:10:01 +0100

    Fixed Caps Lock problem
    - add Focal Fossa Wallpaper

--- capslockfixer-master/fixer.sh
+++ capslockfixer-master/fixer.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-
-# Custom keymap
-
-xkbcomp ~/.config/keymap $DISPLAY
-echo "Valid keyboard setup engaged"
+xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
+    repeat=no,\
+    type[group1]="ALPHABETIC",\
+    symbols[group1]=[ Caps_Lock, Caps_Lock],\
+    actions[group1]=[ LockMods(modifiers=Lock),\
+    Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
+};\
+#' | xkbcomp -w 0 - "$DISPLAY"
