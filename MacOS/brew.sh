02f027d12e2704dc68b8df5d5c382603bcae4cf2
montanarograziano <graziano.montanaro98@gmail.com>
2024-08-27 16:22:21 +0200

    Edit brew script

--- MacOS/brew.sh
+++ MacOS/brew.sh
@@ -27,17 +27,17 @@ if [[ $? != 0 ]]; then
 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
 fi
 
-brew install docker git-delta wget gh nvim ripgrep libpq pipx zsh-syntax-highlighting zsh-autosuggestions 
+brew install docker git-delta wget gh nvim ripgrep libpq pipx zsh-syntax-highlighting zsh-autosuggestions
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code-nerd-font
 brew install --cask font-meslo-lg-nerd-font
+brew install --cask font-iosevka
 brew install --cask whatsapp
 brew install --cask monitorcontrol
 brew install --cask ngrok
 brew install --cask maccy
 brew install --cask visual-studio-code
 brew install --cask keka
-brew install --cask font-iosevka
 brew install --cask rectangle
 brew install --cask jetbrains-toolbox
 brew install --cask discord
@@ -45,7 +45,39 @@ brew install --cask spotify
 
 brew cleanup
 
+# Install Python and Uv
+print "Install uv (https://github.com/astral-sh/uv#uv)"
 
-for lib in "poetry" "pre-commit" "virtualenv" "cookiecutter"; do
-	pipx install "$lib"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+versions=(
+    "3.12"
+    "3.11"
+    "3.10"
+    "3.9"
+)
+
+safe_version="${versions[1]}"
+
+for version in "${versions[@]}"; do
+    uv python install -- "$version"
 done
+
+libs=(
+    "argcomplete"
+    "asitop"
+    "commitizen"
+    "cookiecutter"
+    "huggingface-hub[cli]"
+    "jupytext"
+    "mypy"
+    "posting"
+    "pytest"
+    "ruff"
+)
+
+for lib in "${libs[@]}"; do
+	uv tool install --upgrade --python="$safe_version" -- "$lib"
+done
+
+uv tool install --upgrade --python="$safe_version" --with pre-commit-uv -- pre-commit
