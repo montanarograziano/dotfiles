a0bd136efc53a87b54d0135d40e0b20dd4297815
montanarograziano <graziano.montanaro98@gmail.com>
2024-10-27 16:04:45 +0100

    Add zsh managing to tools.sh

--- MacOS/tools.sh
+++ MacOS/tools.sh
@@ -1,4 +1,37 @@
 #! /bin/zsh
+
+# Set the desired configuration line
+zdotdir_line='export ZDOTDIR="$HOME/.config/zsh"'
+
+# Check if /etc/zshenv exists
+if [[ -f "/etc/zshenv" ]]; then
+	if ! grep -qF "$zdotdir_line" "/etc/zshenv"; then
+		print "$zdotdir_line" | sudo tee -a "/etc/zshenv" >/dev/null
+		print "✔︎ appended '$zdotdir_line' to '/etc/zshenv/'"
+	fi
+else
+	print "$zdotdir_line" | sudo tee "/etc/zshenv" >/dev/null
+	print "✔︎ created '/etc/zshenv' and appended the '$zdotdir_line' line."
+fi
+
+if ! [[ -d "$HOME/.config" ]]; then
+	git clone "https://github.com/montanarograziano/dotfiles" "$HOME/.config"
+else
+	read -r -p "⚠️ '$HOME/.config' already exists. Replace dotfiles? (contents of '$HOME/.config' will not be deleted) [y/N]" response
+	if [[ $response =$HOME (y|yes|Y) ]]; then
+
+		mv "$HOME/.config" "$HOME/.config.bak"
+		print "✔︎ moved old configs to '$HOME/.config.bak'"
+
+		git clone "https://github.com/montanarograziano/dotfiles" "$HOME/.config"
+	else
+		print "⚠️ did not clone dotfiles: this script might fail if a binary is not found"
+	fi
+fi
+
+ln -s "$HOME/.config/.gitconfig" "$HOME/.gitconfig"
+
+
 # Install Python and Uv
 print "Install uv (https://github.com/astral-sh/uv#uv)"
 
