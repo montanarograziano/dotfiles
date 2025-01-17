dd89b57c1faa61fb11881b2d63b7d62e213388fb
montanarograziano <40232320+montanarograziano@users.noreply.github.com>
2025-01-17 12:56:41 +0100

    feat: Add auto_venv function

--- zsh/include/functions.zsh
+++ zsh/include/functions.zsh
@@ -9,6 +9,24 @@ load_dotenv() {
     set +a  # Stop automatically exporting variables
   fi
 }
+
+function auto_venv() {
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+    # If no virtual environment is active and .venv exists, activate it
+    if [[ -d ./.venv ]]; then
+      source ./.venv/bin/activate
+    fi
+  else
+    # If a virtual environment is active, check if the current directory matches
+    parentdir="$(dirname "$VIRTUAL_ENV")"
+    if [[ "$PWD"/ != "$parentdir"/* ]]; then
+      deactivate
+    fi
+  fi
+}
+
 # Hook into the 'chpwd' event to load .env whenever you change directories
 add-zsh-hook chpwd load_dotenv
+add-zsh-hook chpwd auto_venv
 load_dotenv
+auto_venv
